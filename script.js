2cab52c8fa500050e1c21c5855148d57441a7310
deepak chandra <75304854+deepak089@users.noreply.github.com>
2021-05-25 22:55:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const resultDiv = document.querySelector(".result");
+const wordEle = document.querySelector("#word");
+const phonetics = document.querySelector(".phonetics");
+const audio = document.querySelector("audio");
+const wordMeaning = document.querySelector(".word-definition");
+const btn =document.querySelector(".btn");
+const handle = async (e) => {
+  if (e.keyCode === 13) {
+    const word = e.target.value;
+    const result = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+    const data = await result.json();
+    resultDiv.style.display = "block";
+    if (result.ok) {
+      wordEle.innerText = data[0].word;
+      phonetics.innerText = data[0].phonetics[0].text;
+      audio.src = data[0].phonetics[0].audio;
+      wordMeaning.innerText = data[0].meanings[0].definitions[0].definition;
+      const synonyms = data[0].meanings[0].definitions[0].synonyms;
+      let synonymsData = "";
+      for (let i = 0; i < synonyms.length; i++) {
+        synonymsData += `<p class="pills">${synonyms[i]}<p>`;
+      }
+      document.querySelector(".synonyms").innerHTML = synonymsData;
+      return;
+    } else {
+      audio.style.display = "none";
+      document.querySelectorAll(".meaningheading")[0].style.display = "none";
+      document.querySelectorAll(".meaningheading")[1].style.display = "none";
+      wordEle.innerText = data.title;
+      wordMeaning.innerText = data.message;
+    }
+  }
+};
+// code for recognition
+function record() {
+  var recognition = new webkitSpeechRecognition();
+  recognition.lang = "en-GB";
+
+  recognition.onresult = function(event) {
+      // console.log(event);
+      document.getElementById('speechToText').value = event.results[0][0].transcript;
+  }
+  recognition.start();
+
+}
